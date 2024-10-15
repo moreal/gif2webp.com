1eaf39b57d1f1370d52f720a8c44d8fec8fcce36
Moreal <dev.moreal@gmail.com>
2024-10-16 00:09:11 +0900

    Fix wasm filename

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -4,6 +4,26 @@ import isolation from "./.vite/plugins/isolation"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (chunkInfo) => {
+          const noHashFiles = ["vips-es6.js", "vips.wasm"];
+          // @ts-ignore
+          console.log(chunkInfo.names, chunkInfo.originalFileNames);
+          if (chunkInfo.names instanceof Array && chunkInfo.names.length === 1 && noHashFiles.includes(chunkInfo.names[0])) {
+            return "assets/[name].[ext]";
+          }
+
+          // if (noHashFiles.includes(chunkInfo.name)) {
+          //   return "[name].js"; // Keep file unhashed
+          // }
+
+          return "assets/[name]-[hash].[ext]";
+        }
+      }
+    }
+  },
   plugins: [
     react(),
     isolation(),
